3ad7cc14f9efab7d6c2fa09f92cdcb0dd81c45cb
BendedWills <62084567+BendedWills@users.noreply.github.com>
2023-02-23 01:30:45 -0600

    Remove old rendering code

--- Tether.Rendering (Old)/Headers/Tether/Module/Rendering/Common/Defs.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include <Tether/Common/Defs.hpp>
-#include <Tether/Module/Rendering/Vulkan/Common/Defs.hpp>
--- Tether.Rendering (Old)/Source/Compositor.cpp
+++
@@ -1,9 +0,0 @@
-#include <Tether/Module/Rendering/Compositor.hpp>
-
-namespace Tether::Rendering
-{
-	void Compositor::SetClearColor(Math::Vector4f clearColor)
-	{
-		m_ClearColor = clearColor;
-	}
-}
